eec54ca5531a63bb03cccaec59521473f8740aa2
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2022-03-19 19:23:44 +0100

    first simple api backend

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,24 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def get_current_time():
+def test():
     return {'test': 42}
+
+
+@app.route('/search/')
+def search():
+    '''search for entries matching the given filters'''
+    language = request.args.get('language', '')
+    country = request.args.get('country', '')
+    city = request.args.get('city', '')
+    # TODO: query mongoDB
+    return {'results': ['...']}
+
+
+@app.route('/newentry')
+def newentry():
+    return {}
