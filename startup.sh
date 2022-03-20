039f1131c4c3e26284e62fc41962221a1d073183
Jonas Loos <33965649+JonasLoos@users.noreply.github.com>
2022-03-20 01:24:06 +0100

    improve startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,10 @@
+echo "########################## pull updates ##########################"
+git pull
 
+echo "########################## build react app ##########################"
 cd frontend
 npm run build
 
+echo "########################## start server ##########################"
 cd ../backend
 waitress-serve --port=80 "main:app"
