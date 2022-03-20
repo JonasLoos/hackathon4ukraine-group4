ff60538f08978bc3ad58c96bdcd5c1f2df0cb18a
Polina <polina.mikhno@itechart-group.com>
2022-03-20 10:55:19 +0300

    Add Ukrainian translation

--- frontend/src/consts/index.js
+++ frontend/src/consts/index.js
@@ -1,7 +1,9 @@
 import locale_en from "../translations/en.json";
 import locale_ru from "../translations/ru.json";
+import locale_ua from "../translations/ua.json";
 
 export const LOCALE_MAP = {
   en: locale_en,
-  ru: locale_ru
+  ru: locale_ru,
+  ua: locale_ua,
 };
